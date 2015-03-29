fa7f6892c66cb9c536b99b7977baa1586280850c
Thaleia-Dimitra <doudali@gmail.com>
2015-03-29 22:13:05 +0300

    fixed object not found error

--- src/main/java/restaurants/ParseJson.java
+++ src/main/java/restaurants/ParseJson.java
@@ -13,18 +13,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.parser.ParseException;
 
-
 public class ParseJson {
+
 	
-	public static List<Restaurant> restaurantsList = new ArrayList<Restaurant>();
-	
-	public void parseRestaurants() throws JSONException, FileNotFoundException, IOException, ParseException {
+	public ArrayList<Restaurant> parseRestaurants() throws JSONException, FileNotFoundException, IOException, ParseException {
 		
-		JSONArray arr = new JSONArray(new String(Files.readAllBytes(Paths.get("/home/thaleia/Desktop/thesis/items-100.json"))));
+		List<Restaurant> restaurantsList = new ArrayList<Restaurant>();
+		
+		JSONArray arr = new JSONArray(new String(Files.readAllBytes(Paths.get("/home/thaleia/Desktop/thesis/items/items-100.json"))));
 		
 		/*Iterate through all json objects*/
 		for (int i = 0; i < arr.length(); i++) {
-		    			
+					    			
 			String titleJ   = "";
 		    String addressJ = "";
 		    String ratingJ  = "";
@@ -35,63 +35,78 @@ public class ParseJson {
 			
 			try {	
 				JSONObject obj = arr.getJSONObject(i);
-						    
-				/*get title*/
-				JSONArray array = obj.getJSONArray("title");
-				for (int j = 0; j < array.length(); j++) {
-					titleJ = titleJ + array.getString(j);
+				
+				JSONArray array;
+				
+				/*get title*/				
+				if (obj.has("title")) {
+					array= obj.getJSONArray("title");
+					for (int j = 0; j < array.length(); j++) {
+						titleJ = titleJ + array.getString(j);
+					}
+					titleJ = titleJ.replaceAll("\n", "");
 				}
-				titleJ = titleJ.replaceAll("\n", "");
 				
 				/*get address*/
-				array = obj.getJSONArray("address");
-				for (int j = 0; j < array.length(); j++) {
-					addressJ = addressJ + array.getString(j);
+				if (obj.has("address")) {
+					array = obj.getJSONArray("address");
+					for (int j = 0; j < array.length(); j++) {
+						addressJ = addressJ + array.getString(j);
+					}
+					addressJ = addressJ.replaceAll("\n", "");
 				}
-				addressJ = addressJ.replaceAll("\n", "");
 			
 				/*get rating*/
-				array = obj.getJSONArray("rating");
-				for (int j = 0; j < array.length(); j++) {
-					ratingJ = ratingJ + array.getString(j);
+				if (obj.has("rating")) {
+					array = obj.getJSONArray("rating");
+					for (int j = 0; j < array.length(); j++) {
+						ratingJ = ratingJ + array.getString(j);
+					}
+					ratingJ = ratingJ.replaceAll("\n", "");
 				}
-				ratingJ = ratingJ.replaceAll("\n", "");
 			
 				/*get review title*/
-				array = obj.getJSONArray("rtitle");
-				for (int j = 0; j < array.length(); j++) {
-					rtitleJ = rtitleJ + array.getString(j);
+				if (obj.has("rtitle")) {
+					array = obj.getJSONArray("rtitle");
+					for (int j = 0; j < array.length(); j++) {
+						rtitleJ = rtitleJ + array.getString(j);
+					}
+					rtitleJ = rtitleJ.replaceAll("\n", "");
 				}
-				rtitleJ = rtitleJ.replaceAll("\n", "");
 			
 				/*get review*/
-				array = obj.getJSONArray("review");
-				for (int j = 0; j < array.length(); j++) {
-					reviewJ = reviewJ + array.getString(j);
+				if (obj.has("review")) {
+					array = obj.getJSONArray("review");
+					for (int j = 0; j < array.length(); j++) {
+						reviewJ = reviewJ + array.getString(j);
+					}
+					reviewJ = reviewJ.replaceAll("\n", "");
 				}
-				reviewJ = reviewJ.replaceAll("\n", "");
 			
 				/*get longitude*/
-				longitude = obj.getString("longitude");
+				if (obj.has("longitude")) {
+					longitude = obj.getString("longitude");
+				}
 				
 				/*get latitude*/
-				latitude = obj.getString("latitude");
+				if (obj.has("latitude")) {
+					latitude = obj.getString("latitude");
+				}
 
-			} catch (JSONException e) {}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
 			
 			/*create restaurant object*/
 			Restaurant rest = new Restaurant(titleJ, addressJ, ratingJ, rtitleJ, reviewJ, longitude, latitude);
 			//rest.print();
 			restaurantsList.add(rest);
 		}
+		
+		return (ArrayList<Restaurant>) restaurantsList;
 	}
-
-	public static void main(String[] args) throws IOException, ClassNotFoundException  {
-		ParseJson parser = new ParseJson();
-		/*parse json file with restaurants*/
-		try {
-			parser.parseRestaurants();
-		} catch (Exception e) {}
+	
+	public void storeToHBase(ArrayList<Restaurant> restList) throws IOException {
 		
 		/*serialize each restaurant java object to a byte array*/
 		/*Put each serialized restaurant to hbase*/
@@ -101,14 +116,33 @@ public class ParseJson {
 		
 		Serializer ser = new Serializer();
 		
-		for (int i = 0; i < restaurantsList.size(); i++) {
+		for (int i = 0; i < restList.size(); i++) {
 			try {
-				byte[] bytes = ser.serialize(restaurantsList.get(i));
+				byte[] bytes = ser.serialize(restList.get(i));
 				hbaseRest.putHTable(i, bytes, htable);
 				byte[] getBytes = hbaseRest.getHTable(i, htable);
 				Restaurant rst = ser.deserialize(getBytes);
-			} catch (Exception e) {}
+				//rst.print();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+	}
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, Exception  {
+			
+		List<Restaurant> listOfRestaurants = new ArrayList<Restaurant>();
+		
+		ParseJson parser = new ParseJson();
+		//parse json file with restaurants
+		try {
+			listOfRestaurants = parser.parseRestaurants();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		parser.storeToHBase((ArrayList<Restaurant>) listOfRestaurants);
+		
 		System.out.println("all done!");
 	}
 
