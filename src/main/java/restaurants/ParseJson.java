187a869676146bf3764e095ffb604c5b6539d190
Thaleia-Dimitra <doudali@gmail.com>
2015-03-31 17:45:17 +0300

    Now reading file from HDFS and creating restaurants at each new line

--- src/main/java/restaurants/ParseJson.java
+++ src/main/java/restaurants/ParseJson.java
@@ -1,12 +1,17 @@
 package restaurants;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.client.HTable;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,93 +21,104 @@ import org.json.simple.parser.ParseException;
 public class ParseJson {
 
 	
+	public Restaurant returnRestaurant(JSONObject obj) throws JSONException {
+		
+		JSONArray array;
+		String titleJ   = "";
+	    String addressJ = "";
+	    String ratingJ  = "";
+	    String rtitleJ  = "";
+	    String reviewJ  = "";
+		String longitude = "";
+		String latitude  = "";
+		
+		/*get title*/				
+		if (obj.has("title")) {
+			array= obj.getJSONArray("title");
+			for (int j = 0; j < array.length(); j++) {
+				titleJ = titleJ + array.getString(j);
+			}
+			titleJ = titleJ.replaceAll("\n", "");
+		}
+		
+		/*get address*/
+		if (obj.has("address")) {
+			array = obj.getJSONArray("address");
+			for (int j = 0; j < array.length(); j++) {
+				addressJ = addressJ + array.getString(j);
+			}
+			addressJ = addressJ.replaceAll("\n", "");
+		}
+	
+		/*get rating*/
+		if (obj.has("rating")) {
+			array = obj.getJSONArray("rating");
+			for (int j = 0; j < array.length(); j++) {
+				ratingJ = ratingJ + array.getString(j);
+			}
+			ratingJ = ratingJ.replaceAll("\n", "");
+		}
+	
+		/*get review title*/
+		if (obj.has("rtitle")) {
+			array = obj.getJSONArray("rtitle");
+			for (int j = 0; j < array.length(); j++) {
+				rtitleJ = rtitleJ + array.getString(j);
+			}
+			rtitleJ = rtitleJ.replaceAll("\n", "");
+		}
+	
+		/*get review*/
+		if (obj.has("review")) {
+			array = obj.getJSONArray("review");
+			for (int j = 0; j < array.length(); j++) {
+				reviewJ = reviewJ + array.getString(j);
+			}
+			reviewJ = reviewJ.replaceAll("\n", "");
+		}
+	
+		/*get longitude*/
+		if (obj.has("longitude")) {
+			longitude = obj.getString("longitude");
+		}
+		
+		/*get latitude*/
+		if (obj.has("latitude")) {
+			latitude = obj.getString("latitude");
+		}
+		
+		/*create restaurant object*/
+		Restaurant rest = new Restaurant(titleJ, addressJ, ratingJ, rtitleJ, reviewJ, longitude, latitude);
+		
+		return rest;
+		
+	}
+	
 	public ArrayList<Restaurant> parseRestaurants() throws JSONException, FileNotFoundException, IOException, ParseException {
 		
 		List<Restaurant> restaurantsList = new ArrayList<Restaurant>();
 		
-		JSONArray arr = new JSONArray(new String(Files.readAllBytes(Paths.get("/home/thaleia/Desktop/thesis/items/items-100.json"))));
+		//Read local file
+		//JSONArray arr = new JSONArray(new String(Files.readAllBytes(Paths.get("/home/thaleia/Desktop/thesis/items/items-100.json"))));
 		
-		/*Iterate through all json objects*/
-		for (int i = 0; i < arr.length(); i++) {
-					    			
-			String titleJ   = "";
-		    String addressJ = "";
-		    String ratingJ  = "";
-		    String rtitleJ  = "";
-		    String reviewJ  = "";
-			String longitude = "";
-			String latitude  = "";
-			
-			try {	
-				JSONObject obj = arr.getJSONObject(i);
-				
-				JSONArray array;
-				
-				/*get title*/				
-				if (obj.has("title")) {
-					array= obj.getJSONArray("title");
-					for (int j = 0; j < array.length(); j++) {
-						titleJ = titleJ + array.getString(j);
-					}
-					titleJ = titleJ.replaceAll("\n", "");
-				}
-				
-				/*get address*/
-				if (obj.has("address")) {
-					array = obj.getJSONArray("address");
-					for (int j = 0; j < array.length(); j++) {
-						addressJ = addressJ + array.getString(j);
-					}
-					addressJ = addressJ.replaceAll("\n", "");
-				}
-			
-				/*get rating*/
-				if (obj.has("rating")) {
-					array = obj.getJSONArray("rating");
-					for (int j = 0; j < array.length(); j++) {
-						ratingJ = ratingJ + array.getString(j);
-					}
-					ratingJ = ratingJ.replaceAll("\n", "");
-				}
-			
-				/*get review title*/
-				if (obj.has("rtitle")) {
-					array = obj.getJSONArray("rtitle");
-					for (int j = 0; j < array.length(); j++) {
-						rtitleJ = rtitleJ + array.getString(j);
-					}
-					rtitleJ = rtitleJ.replaceAll("\n", "");
-				}
-			
-				/*get review*/
-				if (obj.has("review")) {
-					array = obj.getJSONArray("review");
-					for (int j = 0; j < array.length(); j++) {
-						reviewJ = reviewJ + array.getString(j);
-					}
-					reviewJ = reviewJ.replaceAll("\n", "");
-				}
-			
-				/*get longitude*/
-				if (obj.has("longitude")) {
-					longitude = obj.getString("longitude");
-				}
-				
-				/*get latitude*/
-				if (obj.has("latitude")) {
-					latitude = obj.getString("latitude");
-				}
-
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			
-			/*create restaurant object*/
-			Restaurant rest = new Restaurant(titleJ, addressJ, ratingJ, rtitleJ, reviewJ, longitude, latitude);
-			//rest.print();
-			restaurantsList.add(rest);
+		//Read file on HDFS
+		try{
+			Path pt=new Path("/data/items-100-new.json");
+            FileSystem fs = FileSystem.get(new Configuration());
+            BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
+            String line;
+            line=br.readLine();
+            while (line != null){
+				JSONObject obj = new JSONObject(line);
+				Restaurant rest = returnRestaurant(obj);
+				//rest.print();
+				restaurantsList.add(rest);
+            	line=br.readLine();
+            }
+		} catch(Exception e){
+			e.printStackTrace();
 		}
-		
+	
 		return (ArrayList<Restaurant>) restaurantsList;
 	}
 	
