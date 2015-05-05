c49e98b9dd07710204197fe41c205ac2be753261
Thaleia-DimitraDoudali <doudali@gmail.com>
2015-05-05 14:53:55 +0300

    determine number of check-in's per user using gaussian

--- src/main/java/checkIns/CreateCheckIn.java
+++ src/main/java/checkIns/CreateCheckIn.java
@@ -2,21 +2,34 @@ package checkIns;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import restaurants.ParseJson;
 import restaurants.Restaurant;
 
 public class CreateCheckIn {
 	
-	List<Restaurant> restaurants = new ArrayList<Restaurant>();
+	private List<Restaurant> restaurants = new ArrayList<Restaurant>();
+	private List<Integer> users = new ArrayList<Integer>();
 
-
-	public CreateCheckIn(String file) {
+	public CreateCheckIn(String file, int userNum, int mean, int dev) {
+		
 		createRestaurants(file);
-		for (int i = 0; i < restaurants.size(); i++) {
-			System.out.println("----------------" + (i+1) + "------------------");
-			restaurants.get(i).print();
+		
+		for (int i = 1; i <= userNum; i++) {
+			System.out.print("User no." + i + ":");
+			users.add(i);
+			//how many check-in's per user?
+			int checkNum = createGaussian(mean, dev);
+			System.out.println("\tnumber of check-ins: " + checkNum);
 		}
+	}
+	
+	public int createGaussian(int mean, int dev) {
+		Random r = new Random();
+		double val = r.nextGaussian()*dev + mean;
+		int res = (int) Math.round(val);
+		return res;
 	}
 	
 	public void createRestaurants(String file) {
@@ -30,8 +43,20 @@ public class CreateCheckIn {
 		}
 	}
 
+	public List<Restaurant> getRestaurants() {
+		return restaurants;
+	}
+
+	public void setRestaurants(List<Restaurant> restaurants) {
+		this.restaurants = restaurants;
+	}
+
 	public static void main(String[] args) {
-		new CreateCheckIn(args[0]);
+		//args[0]: json file with restaurants
+		//args[1]: number of users
+		//args[2], args[3]: mean and standard deviation for the gaussian that determines that determines the number
+		//of check-in's per user
+		new CreateCheckIn(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
 	}
 
 }
--- src/main/scripts/test.sh
+++ src/main/scripts/test.sh
@@ -9,5 +9,5 @@ JAR_FILE="target/thesis-maven-0.0.1-SNAPSHOT.jar"
 
 CLASSPATH=$HBASE_CLS:$LOCAL_CLS:$JAR_FILE
 
-echo $CLASSPATH
-java -cp $CLASSPATH $1 $2 
+##echo $CLASSPATH
+java -cp $CLASSPATH $1 $2 $3 $4 $5
