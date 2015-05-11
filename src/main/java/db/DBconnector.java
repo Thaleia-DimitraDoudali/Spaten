6d38747c14c1381a5a6eb81f64ea23e304fe9f34
Thaleia-DimitraDoudali <doudali@gmail.com>
2015-05-11 18:54:02 +0300

    creating table, inserting value - simple version

--- src/main/java/db/DBconnector.java
+++ src/main/java/db/DBconnector.java
@@ -3,8 +3,13 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBconnector {
+	
+	private Connection connection = null;
+	private Statement statement = null;
+
 
 	public DBconnector() {
 		// TODO Auto-generated constructor stub
@@ -16,20 +21,56 @@ public class DBconnector {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		Connection connection = null;
 		try {
 			connection = DriverManager.getConnection(
 			   "jdbc:postgresql:datagen","postgres", "root");
-			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		System.out.println("Connected to PostgreSQL...");
 	}
 	
+	public void createTable() {
+	    try {
+	    	statement = connection.createStatement();	      
+	    	String sql = "CREATE TABLE pois(poisId INT NOT NULL PRIMARY KEY,"
+	    			+ " poi POINT);" ;
+			statement.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Created table 'pois'...");
+	}
+	
+	public void dropTable() {
+	    try {
+	    	statement = connection.createStatement();	      
+	    	String sql = "DROP TABLE pois;";
+	    	statement.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	    	
+	}
+	
+	public void insert(int id, String lng, String lat) {
+	    try {
+	    	statement = connection.createStatement();	      
+	    	String sql = "INSERT INTO pois (poisId, poi) VALUES ("
+	    			+ id + ", POINT(" + lng + ", " + lat   
+	    			+ ") );" ;
+			statement.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Inserted record on 'pois'...");
+	}
+	
 	public static void main(String[] args) {
 		DBconnector db = new DBconnector();
 		db.connect();
+		db.createTable();
+		db.insert(1, "34.20332", "45.02345");
+		//db.dropTable();
 	}
 
 }
