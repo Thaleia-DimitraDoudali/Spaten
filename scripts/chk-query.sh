61f3dbc7bc954a169c84e49a07bbf29b0c529d04
Thaleia-DimitraDoudali <doudali@gmail.com>
2015-07-30 21:31:16 +0300

    adding indicative source and generated data files

--- scripts/chk-query.sh
+++ scripts/chk-query.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Usage $1 = <number of concurrent queries> $2 = <usersNo>
+
 #Put code jar in hbase lib folder!
 export HBASE_CLASSPATH=$(/opt/hbase-0.94.27/bin/hbase classpath)
 nohup java -cp java -cp $HBASE_CLASSPATH client.CheckInsQuery $1 $2 > chk-query.out &
